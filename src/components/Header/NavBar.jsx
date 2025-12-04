1734ba28bce8bf4bb63cbce3268b3d94a53e36d3
miaravnanger <mia_maroen@hotmail.com>
2025-12-04 16:50:37 +0100

    change to hashbrowser to work on github pages

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -19,27 +19,33 @@ const categories = [
   "philosophy",
 ];
 
-export default function NavBar({ onHomeClick}) {
+export default function NavBar({ onHomeClick }) {
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleCategorySelect = (category) => {
-    navigate(`/gutendex2/category/${category}`);
+    navigate(`category/${category}`);
     setIsModalOpen(false);
   };
 
   const handleHome = () => {
-    onHomeClick();  //reset input and search data
-    navigate("/gutendex2/");
+    onHomeClick(); //reset input and search data
+    navigate("/");
   };
 
   return (
     <nav className="navBar">
-      <button className="button" onClick={() => setIsModalOpen(true)}>Categories</button>
-      <Link to="favorites" className="button">Favorites</Link>
+      <button className="button" onClick={() => setIsModalOpen(true)}>
+        Categories
+      </button>
+      <Link to="favorites" className="button">
+        Favorites
+      </Link>
       {/* button to home insted of link because i want to reset the search input */}
-      <button className="button" onClick={handleHome}>Home</button>
-      <BookModal 
+      <button className="button" onClick={handleHome}>
+        Home
+      </button>
+      <BookModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         categories={categories}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,21 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import Favorites from "../pages/Favorites";
 import CategoryBooks from "../pages/CategoryBooks";
 import BookModal from "../pages/BookDetailsModal";
 
-
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/gutendex2/",
+    path: "/",
     element: <App />,
     errorElement: <h1>an error occured</h1>,
     children: [
       { index: true, element: <Home /> },
       { path: "category/:category", element: <CategoryBooks /> },
       { path: "favorites", element: <Favorites /> },
-      {path: "book/:bookId", element: <BookModal/>},
+      { path: "book/:bookId", element: <BookModal /> },
       { path: "*", element: <h1>404 not found</h1> },
     ],
   },
